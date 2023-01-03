e84c7327d6cba049a7ca252850a6043261ca7731
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2023-01-03 07:29:09 +0100

    fix bugs

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Contact from "../../components/Contact";
 import Confirmbooking from "../../components/Confirmbooking";
-import OtherInfo from "./Other";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { useRouter } from "next/router";
