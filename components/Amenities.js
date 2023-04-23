90aec2cf5eac84f605397bec5b0d4f7cbcc25ad8
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2023-04-23 14:58:07 +0100

    push

--- components/Amenities.js
+++ components/Amenities.js
@@ -4,7 +4,7 @@ const Amenities = ({ amenities }) => {
   return (
     <>
       <div className="grid justify-between lg:grid-cols-6 grid-cols-3 gap-4 font-poppins">
-        {amenities.map((amenity) => (
+        {amenities && amenities.map((amenity) => (
           <div className="flex flex-col items-center space-y-1">
             <span className="p-3 bg-pri-main/20 rounded-full">
               <i className={`fa fa-${amenity.icon} text-lg text-[#f5c400]`}></i>
